c03f5d803deed7c21c68d27629269ae176b8a939
m305-58\w401 <tommy555553@gmail.com>
2018-04-26 17:26:33 +0800

    hi

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> number = new List<int>();
+
+
+            for (int i = 1; i < 43; i++)
+            {
+                number.Add(i);
+            }
+
+
+            Random rand = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+
+                int b = rand.Next(1, number.Count);
+                Console.Write(number[b] + " ");
+
+
+                number.RemoveAt(b);
+            }
+
+
+            Console.Read();
+        }
+    }
+}
